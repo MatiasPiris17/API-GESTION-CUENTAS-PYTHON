7c0df3dcd2411105a9488adab3c8abc08ead45fc
matias piris <matiaslinkedin17@gmail.com>
2024-03-27 12:08:17 -0300

    dockerfile config

--- microservicio-cuentas/main.py
+++ microservicio-cuentas/main.py
@@ -6,4 +6,5 @@ app = Sanic("Accounts")
 app.add_route(Accounts.as_view(), "/accounts")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    # app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
--- microservicio-transacciones/main.py
+++ microservicio-transacciones/main.py
@@ -6,4 +6,5 @@ app = Sanic("Transactions")
 app.add_route(Transactions.as_view(), "/transactions")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8081, debug=True)
+    # app.run(host="127.0.0.1", port=8081, debug=True)
+    app.run(host='0.0.0.0', port=8001)
