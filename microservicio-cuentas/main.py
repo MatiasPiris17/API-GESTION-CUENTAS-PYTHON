d6cdf4aba91cc8f56e445706c75e6b74c1a3d4fb
matias piris <matiaslinkedin17@gmail.com>
2024-03-27 10:06:26 -0300

    Account controller added to routes

--- microservicio-cuentas/main.py
+++ microservicio-cuentas/main.py
@@ -1,11 +1,9 @@
 from sanic import Sanic
-from sanic.response import text
+from src.controller import Accounts
 
 app = Sanic("accounts")
-
-@app.get("/")
-async def hello_world(request):
-    return text("Hello, accounts.")
+    
+app.add_route(Accounts.as_view(), "/accounts")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
