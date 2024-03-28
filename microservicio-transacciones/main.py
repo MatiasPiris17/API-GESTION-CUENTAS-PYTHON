cb8f58934a49f726fc83b98935f2e5124fa0bea4
Matias Piris <matiaslikedin17@gmail.com>
2024-03-28 16:36:18 -0300

    docker config

--- microservicio-transacciones/main.py
+++ microservicio-transacciones/main.py
@@ -12,7 +12,7 @@ def main() :
     app.add_route(transferController.as_view(), "/transfer")
 
     if __name__ == "__main__":
-        app.run(host="127.0.0.1", port=8081, debug=True)
+        app.run(host="127.0.0.1", port=8001, debug=True)
         # app.run(host='0.0.0.0', port=8001)
     # connection.close()
 
